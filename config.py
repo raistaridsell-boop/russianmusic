a5c9d78d8f9d4e9226748db518077e10a1dfce14
raistaridsell-boop <raistaridsell@gmail.com>
2026-07-26 22:22:09 +0530

    Update config.py
    Signed-off-by: raistaridsell-boop <raistaridsell@gmail.com>

--- config.py
+++ config.py
@@ -10,7 +10,10 @@ API_ID = int(getenv("32470626"))
 API_HASH = getenv("1d701ebaf57eef0c94400072ae752328")
 BOT_TOKEN = getenv("8967952528:AAHSdbXbWgDhBS3Gb-pO8S0-9IEGf1n-Clk")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://akashrajput010208:qy9qMomvLOpbegOP@cluster0.21upfwm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+MONGO_DB_URI = getenv(
+    "MONGO_DB_URI",
+    "mongodb+srv://akashrajput010208:qy9qMomvLOpbegOP@cluster0.21upfwm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+))
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000))
 
